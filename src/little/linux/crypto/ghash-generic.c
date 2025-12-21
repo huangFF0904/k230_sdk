7a3284c3b64d5d21191a6df7194fe547dcbe1054
huangFF0904 <625173hhh@gmail.com>
2025-12-21 23:50:01 +0800

    crypto: ghash - Avoid null pointer dereference if no key is set

--- src/little/linux/crypto/ghash-generic.c
+++ src/little/linux/crypto/ghash-generic.c
@@ -134,6 +134,9 @@ static int ghash_final(struct shash_desc *desc, u8 *dst)
 	struct ghash_ctx *ctx = crypto_shash_ctx(desc->tfm);
 	u8 *buf = dctx->buffer;
 
+	if (!ctx->gf128)
+		return -ENOKEY;
+
 	ghash_flush(ctx, dctx);
 	memcpy(dst, buf, GHASH_BLOCK_SIZE);
 
